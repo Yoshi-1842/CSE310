b2a92926607824c3ce4eef13206fb56d3070a1c8
Yosef Pineda <80504074+Yoshi-1842@users.noreply.github.com>
2023-09-25 03:54:15 +0000

    merge sort correct

--- merge_sort/main.cpp
+++ merge_sort/main.cpp
@@ -19,8 +19,9 @@ int main()
 
     // Elements in Array
     long long int arr[ number_of_elements ];
+    
     for (int index = 0; index < number_of_elements; index ++)
-        cin >> arr[ index ];
+      cin >> arr[ index ];
 
     merge_sort (arr, 0, number_of_elements - 1);
     print_array (arr, number_of_elements);
--- merge_sort/merge_sort.cpp
+++ merge_sort/merge_sort.cpp
@@ -7,55 +7,53 @@ using namespace std;
 void merge_two_sorted_arrays(long long int array[], int const left, int const mid, int const right) 
 {
     // Sub-array sizes
-	int const sub_array_one = mid - left + 1;// length of Array[left to mid]
-	int const sub_array_two = right - mid; //length of Array[mid + 1 to right]
+    int const sub_array_one = mid - left + 1;  // Length of Array[left to mid]
+    int const sub_array_two = right - mid;      // Length of Array[mid + 1 to right]
 
-	// Create temp arrays
-	long long int *left_array = new long long int[sub_array_one];
-	long long int *right_array = new long long int[sub_array_two];
+    // Create temp arrays
+    long long int *left_array = new long long int[sub_array_one];
+    long long int *right_array = new long long int[sub_array_two];
 
-	/* Copy data to temp arrays leftArray[] and rightArray[] */
-        for(int i = 0; i< sub_array_one; i++){
-			left_array[i] = array[left + 1];
-		}
-		for(int j=0;j< sub_array_two;j++){
-			right_array[j] = array[mid + j + 1];
-		}
+    /* Copy data to temp arrays left_array[] and right_array[] */
+    for (int i = 0; i < sub_array_one; i++) {
+        left_array[i] = array[left + i];  // Corrected indexing
+    }
+    for (int j = 0; j < sub_array_two; j++) {
+        right_array[j] = array[mid + j + 1];  // Corrected indexing
+    }
 
-    /* Indices of the left, right and merged array*/
-        
-		int indexSub_Aone =0;//i
-		int indexSub_Atwo = 0;//j
-		int indexMerge_A = left;//k
+    /* Indices of the left, right, and merged array */
+    int indexSub_Aone = 0;  // i
+    int indexSub_Atwo = 0;  // j
+    int indexMerge_A = left;  // k
 
+    /* Merge the temp arrays back into array[left..right] */
+    while (indexSub_Aone < sub_array_one && indexSub_Atwo < sub_array_two) {
+        if (left_array[indexSub_Aone] <= right_array[indexSub_Atwo]) {
+            array[indexMerge_A] = left_array[indexSub_Aone];
+            indexSub_Aone += 1;
+        } else {
+            array[indexMerge_A] = right_array[indexSub_Atwo];
+            indexSub_Atwo += 1;
+        }
+        indexMerge_A += 1;
+    }
 
-	/* Merge the temp arrays back into array[left..right] */
-        while(indexSub_Aone < sub_array_one && indexSub_Atwo < sub_array_two){
-			if(left_array[indexSub_Aone] <= right_array[indexSub_Atwo]){
-				array[indexMerge_A] = left_array[indexSub_Aone];
-				indexSub_Aone += 1;
-			}else{
-				array[indexMerge_A] = right_array[indexSub_Atwo];
-				indexSub_Atwo += 1;
-			}
-			indexMerge_A += 1;
-		}
-	/* Copy the remaining elements of
-	   left[], if there are any */
-        while(indexSub_Aone < sub_array_one){
-			array[indexMerge_A] = left_array[indexSub_Aone];
-			indexSub_Aone += 1;
-			indexMerge_A +=1;
-		}
+    /* Copy the remaining elements of left[], if there are any */
+    while (indexSub_Aone < sub_array_one) {
+        array[indexMerge_A] = left_array[indexSub_Aone];
+        indexSub_Aone += 1;
+        indexMerge_A += 1;
+    }
 
-	/* Copy the remaining elements of
-	   right[], if there are any */
-       while(indexSub_Atwo < sub_array_two){
-		array[indexMerge_A] = right_array[indexSub_Atwo];
-		indexSub_Atwo += 1;
-		indexMerge_A +=1;
-	   }
-	// Delete left and right sub-arrays
+    /* Copy the remaining elements of right[], if there are any */
+    while (indexSub_Atwo < sub_array_two) {
+        array[indexMerge_A] = right_array[indexSub_Atwo];
+        indexSub_Atwo += 1;
+        indexMerge_A += 1;
+    }
+
+    // Delete left and right sub-arrays
     delete[] left_array;
     delete[] right_array;
 }
@@ -73,6 +71,5 @@ void merge_sort(long long int array[], int const begin, int const end)
 
 		
 		merge_two_sorted_arrays(array, begin, q, end);
-
  
 }
