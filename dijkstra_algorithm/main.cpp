7d503d2b7b7d4223cc2eecfb7f3ebd2e0f1b484f
Yosef Pineda <80504074+Yoshi-1842@users.noreply.github.com>
2023-11-30 17:34:14 +0000

    Implementation of Minheap and Main function

--- dijkstra_algorithm/main.cpp
+++ dijkstra_algorithm/main.cpp
@@ -1,46 +1,51 @@
-#include "graph.h"
+#include <iostream>
+#include <stdexcept>  // 
+#include "graph.h"   // Assuming you have a header file for the Graph class
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
-    /*
+    // Command-line argument validation
+    if (argc < 3) {
+        cerr << "Usage: " << argv[0] << " <graph_filename> <direction>" << endl;
+        return EXIT_FAILURE;
+    }
+    
     // Extract graph filename and direction from command-line arguments
-    graphFilename = argv[1]
-    direction = argv[2]
+    char* graphFilename = argv[1];
+    char* direction = argv[2];
 
     // Create an instance of the Graph class
-    graph = new Graph()
+    Graph graph;
 
     // Load the graph from the specified file
-    if not graph.loadGraph(graphFilename, direction)
-        print "Failed to load the graph."
-        return 1
+    if (!graph.loadGraph(graphFilename, direction)) {
+        cerr << "Failed to load the graph from file: " << graphFilename << endl;
+        return EXIT_FAILURE;
+    }
 
-    // Process queries until the "stop" query is received
-    while true
-        // Read the next query from the user
-        query = readInput()
+    while (true) {
+        string query, source, destination, flag, action;
+        cout << "Enter a query: ";
+        cin >> query >> source >> destination >> flag >> action;
 
-        // Check the type of query
-        if query is "find"
-            // Read source, destination, and flag from user
-            source, destination, flag = readInput()
-
-            // Run Dijkstra's algorithm on the graph
-            graph.runDijkstra(source, destination, flag)
-
-        else if query is "write"
-            // Read action, source, and destination from user
-            action, source, destination = readInput()
-
-            // Write the path based on the action
-            graph.writePath(source, destination)
-
-        else if query is "stop"
-            // Exit the program
-            return 0
-
-        else
-            // Invalid query, print an error message
-            print "Invalid query. Please use 'find', 'write', or 'stop'."
-
-    */
+        if (query == "find") {
+            try {
+                graph.runDijkstra(stoi(source), stoi(destination), stoi(flag));
+            } catch (const invalid_argument& e) {
+                cerr << "Invalid input for find query. Please enter valid integers." << endl;
+            }
+        } else if (query == "write") {
+            try {
+                graph.writePath(stoi(source), stoi(destination));
+            } catch (const invalid_argument& e) {
+                cerr << "Invalid input for write query. Please enter valid integers." << endl;
+            }
+        } else if (query == "stop") {
+            return EXIT_SUCCESS;
+        } else {
+            cout << "Invalid query. Please use 'find', 'write', or 'stop'." << endl;
+        }
+    }
+    
 }
--- dijkstra_algorithm/minheap.cpp
+++ dijkstra_algorithm/minheap.cpp
@@ -1,62 +1,110 @@
 #include "minheap.h"
 
 MinHeap::MinHeap() {
-    //set pointers to nullptr
-    //set variables to default values
+    // Set pointers to nullptr
+    // Set variables to default values
+    capacity = 0;
+    size = 0;
+    keys = nullptr;
+    values = nullptr;
 }
 
-void MinHeap::init(int size) {
-    //set capacity and size
-    //allocate memory for keys(weights) and values(nodes)
+void MinHeap::init(int newSize) {
+    // Set capacity and size
+    capacity = newSize;
+    size = 0;
+
+    // Allocate memory for keys (weights) and values (nodes)
+    keys = new double[capacity];
+    values = new int[capacity];
 }
 
 void MinHeap::push(double key, int value) {
-    /*
-    if heap is full
-        return
+    // If the heap is full
+    if (size == capacity) {
+        return;
+    }
 
-    increase size
-    set key and value at the end with index as size
-    heapify up
-    */
+    // Increase size
+    size++;
+
+    // Set key and value at the end with the index as size
+    keys[size] = key;
+    values[size] = value;
+
+    // Heapify up
+    heapifyUp(size);
 }
 
 int MinHeap::pop() {
-    /*
-    if heap is empty
-        return sentinel value
+    // If the heap is empty
+    if (size == 0) {
+        // Return sentinel value (assuming some sentinel value like -1)
+        return -1;
+    }
 
-    get root value
-    replace root with the last element
-    decrease size
-    heapify down
+    // Get root value
+    int rootValue = values[1];
 
-    return root value
-    */
+    // Replace root with the last element
+    values[1] = values[size];
+    keys[1] = keys[size];
+
+    // Decrease size
+    size--;
+
+    // Heapify down
+    heapifyDown(1);
+
+    return rootValue;
 }
 
 bool MinHeap::empty() const {
-    //return true if size is 0, false otherwise
+    // Return true if size is 0, false otherwise
+    return size == 0;
 }
 
 void MinHeap::heapifyUp(int index) {
-    /*
-    while index > 1 and current key is less than parent key (parent key is index / 2)
-        swap index with parent
-        update index to parent
-    */
+    // While index > 1 and the current key is less than the parent key
+    while (index > 1 && keys[index] < keys[index / 2]) {
+        // Swap index with parent
+        swap(index, index / 2);
+
+        // Update index to parent
+        index = index / 2;
+    }
 }
 
 void MinHeap::heapifyDown(int index) {
-    /*
-    find the smallest child from left(2*index) or right(2*index+1) child of index
-    if smallest child is smaller than current key
-        swap with smallest child with index
-        update index
-        heapify down
-    */
+    // Find the smallest child from left (2*index) or right (2*index+1) child of index
+    int smallestChild = index * 2;
+    
+
+    if (smallestChild + 1 <= size && keys[smallestChild + 1] < keys[smallestChild]) {
+        smallestChild = smallestChild + 1;
+    }
+
+    // If the smallest child is smaller than the current key
+    if (smallestChild <= size && keys[smallestChild] < keys[index]) {
+        // Swap with the smallest child
+        swap(index, smallestChild);
+
+        // Update index
+        index = smallestChild;
+
+        // Heapify down
+        heapifyDown(index);
+    }
 }
 
 void MinHeap::swap(int i, int j) {
-    //swap keys and values at positions i and j
+    // Swap keys and values at positions i and j
+    double tempKey = keys[i];
+    int tempValue = values[i];
+
+    keys[i] = keys[j];
+    values[i] = values[j];
+
+    keys[j] = tempKey;
+    values[j] = tempValue;
 }
